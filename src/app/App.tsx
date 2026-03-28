b4d4a8e0db5e6665f4d0765fd909ff77c4ff86e9
rakeshlakshmanan <ralakshm@tcd.ie>
2026-03-28 21:03:43 +0000

    Add Stats section between Hero and About

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,12 +6,14 @@ import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
 import { ParticleBackground } from "./components/ParticleBackground";
+import { Stats } from "./components/Stats";
 
 export default function App() {
   return (
     <div className="size-full bg-black text-white">
       <ParticleBackground />
       <Hero />
+      <Stats />
       <About />
       <Experience />
       <Skills />
